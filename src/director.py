211b0fb195aebe2d2bb32999c65ad0bbcfdd35fc
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-08-02 16:04:25 +0200

    add artibut et methods pour avoir les cours et courses et student

--- src/director.py
+++ src/director.py
@@ -1,8 +1,7 @@
-from dataclasses import dataclass
 
-
+from typing import List
+from dataclasses import dataclass, field
 from person import Person
-from address import Address
 from student import Student
 from teacher import Teacher
 from course import Course
@@ -10,4 +9,16 @@ from course import Course
 
 @dataclass
 class Director(Person):
-    
+
+        students: List[Student] = field(default_factory=list)
+        teachers: List[Teacher] = field(default_factory=list)
+        courses: List[Course] = field(default_factory=list)
+
+  def get_students(self) -> List[Student]:
+            return self.students
+
+ def get_teachers(self) -> List[Teacher]:
+            return self.teachers
+
+ def get_courses(self) -> List[Course]:
+            return self.courses
