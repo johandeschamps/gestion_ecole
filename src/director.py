6c859a5c610aa87d6030d54b3553c8f8e27631d7
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-08-05 09:56:12 +0200

    add method of modifier les course

--- src/director.py
+++ src/director.py
@@ -18,7 +18,7 @@ def get_students(self) -> List[Student]:
              return  self.students
 
 
- def get_teachers(self) -> List[Teacher]:
+def get_teachers(self) -> List[Teacher]:
             return self.teachers
 
 
@@ -33,11 +33,11 @@ def get_courses(self) -> List[Course]:
 def assign_course(self, teacher: Teacher, course: Course):
     # Check if the teacher is already in the list of teachers
     if teacher not in self.teachers:
-        raise ValueError("The teacher is not registered with this director.")
+        raise ValueError("The teacher is not registered.")
 
     # Check if the course is already in the list of courses
     if course not in self.courses:
-        raise ValueError("The course is not registered with this director.")
+        raise ValueError("The course is not registered.")
 
     # Assign the course to the teacher
     if course not in teacher.courses:
@@ -48,3 +48,15 @@ def assign_course(self, teacher: Teacher, course: Course):
     # Optional: Add the course to the director's list of courses if it's not already present
     if course not in self.courses:
         self.courses.append(course)
+
+
+def modify_course(self, course: Course, name: str , start_date: str , end_date: str ):
+    if course in self.courses:
+        if name:
+            course.name = name
+        if start_date:
+            course.start_date = start_date
+        if end_date:
+            course.end_date = end_date
+    else:
+        raise ValueError("this course is not already registered")
